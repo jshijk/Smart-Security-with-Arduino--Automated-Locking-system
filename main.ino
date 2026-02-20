ef6f2acb350faeb6ae661b277e73a259130a2468
jshijk <joshrylbalasabas-it@srcb.edu.ph>
2026-02-20 13:03:50 +0800

    Main branch
    Please kindly check if naa paba kulang or naa mo idugang

---
+++ main.ino
@@ -0,0 +1,76 @@
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+String UID = "83 B9 BE 18";
+byte lock = 1;
+
+const int RELAY_PIN = 7;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+
+void setup() {
+  Serial.begin(9600);
+   pinMode(RELAY_PIN, OUTPUT);
+
+  lcd.init();
+  lcd.backlight();
+
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void loop() {
+  lcd.setCursor(4, 0);
+  lcd.print("Welcome!");
+  lcd.setCursor(1, 1);
+  lcd.print("Put your card");
+
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Scanning");
+  Serial.print("NUID tag is :");
+  String ID = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    lcd.print(".");
+    ID.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    ID.concat(String(rfid.uid.uidByte[i], HEX));
+    delay(300);
+  }
+  ID.toUpperCase();
+
+  if (ID.substring(1) == UID && lock == 0 ) {
+   
+    digitalWrite(RELAY_PIN,LOW);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Door is locked");
+    delay(1500);
+    lcd.clear();
+    lock = 1;
+  } else if (ID.substring(1) == UID && lock == 1 ) {
+    
+    digitalWrite(RELAY_PIN,HIGH);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Door is open");
+    delay(1500);
+    lcd.clear();
+    lock = 0;
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Wrong card!");
+    delay(1500);
+    lcd.clear();
+  }
+}
